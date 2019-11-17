22c3be16aaf41faf95683bf4594682083c06e047
世豪 赵 <2981047480@qq.com>
2019-11-17 20:27:57 +0800

    。。。

--- 大二机器学习记录/泰坦尼克数据/算法.py
+++ 大二机器学习记录/泰坦尼克数据/算法.py
@@ -1,1 +1,70 @@
-import sklearn as sk
+import warnings#忽略警告提示
+warnings.filterwarnings('ignore')
+
+import numpy as np
+import pandas as pd
+
+train=pd.read_csv('train.csv')
+test=pd.read_csv('test.csv')
+print('训练数据集：',train.shape,'测试数据集',test.shape)
+
+rowNum_train=train.shape[0]
+rowNum_test=test.shape[0]
+print('训练集行数：',rowNum_train)
+print('测试集行数',rowNum_test)
+
+full_df=train.append(test,ignore_index=True)
+print('合并后的数据集：',full_df.shape)
+
+print(full_df.head())#打印前几行
+print(full_df.describe())#查看数据的统计信息
+print(full_df.info())#查看每一列的数据类型，数据总数，以及是否有数据缺失
+
+full_df['Fare']=full_df['Fare'].fillna(full_df['Fare'].mean())
+full_df['Age']=full_df['Age'].fillna(full_df['Age'].mean())#把年龄和费用（票价）用均值来替代
+print(full_df['Embarked'].mode())#查看Embark这一列的众数
+full_df['Embarked']=full_df['Embarked'].fillna(full_df['Embarked'].mode())
+
+print(full_df.info())
+sex_mapDict={'male':1,'female':0}
+full_df['Sex']=full_df['Sex'].map(sex_mapDict)
+print(full_df.head())
+
+embarkedDF=pd.DataFrame()
+embarkedDF=pd.get_dummies(full_df['Embarked'],prefix='Embarked')#使用get_dummies进行one-hot编码，列名前缀是Embarked
+print(embarkedDF.head())
+
+full=pd.concat([full_df,embarkedDF],axis=1)#因为这里用了登船港口(Embarked)进行了one-hot编码产生了它的虚拟变量，所以这里把它删掉
+full.drop('Embarked',axis=1,inplace=True)
+print(full.head())
+print(full.shape)
+
+name1='Braund, Mr. Owen Harris'
+str1=name1.split(',')[1]
+str2=str1.split('.')[0]
+str3=str2.split()
+
+def getTitle(name):
+    str1 = name1.split(',')[1]
+    str2 = str1.split('.')[0]
+    str3 = str2.strip()
+    return str3
+
+
+
+titleDF=pd.DataFrame()
+titleDF['Title']=full['Name'].map(getTitle)
+print(titleDF.head())
+
+# full=pd.concat([full,titleDF],axis=1)
+#
+# full.drop('Name',axis=1,inplace=True)
+# print(full.head)
+#
+# corrDF=full.corr()
+# print(corrDF)
+#
+# print(corrDF['Survived'].sort_values(ascending=False))
+#
+# full_X=pd.concat([titleDF],axis=1)
+# print(full_X.head())
--- 算法题/Project1/源.cpp
+++ 算法题/Project1/源.cpp
@@ -1,11 +1,162 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#define N 6
+#define M 2*N-1
+typedef struct node
+{
+	int data;
+	int P;//双亲
+	int L;//左孩子
+	int R;//右孩子
+}BT;
+typedef struct Hnode
+{
+	char ch[N + 1];
+	char cd[N + 1][5];
+	BT HFM[M + 1];//0不用
+}HBT;
 
+void new_HFM(HBT* H);//建立哈夫曼树
+void min_s1s2(HBT* H, int n, int* s1, int* s2);//求最小两个值
+void cd(HBT* H, char a[]);//编码
+void key(HBT* H, char b[]);//译码
 int main()
 {
-	int a[3] = { 1,2,3 };
-	int len = sizeof(a) / sizeof(int);
-	printf("%d", len);
-	system("pause");
+	int i, q;
+	char a[5], b[20];
+	HBT* H = (HBT*)malloc(sizeof(HBT));
+	for (i = 1; i <= N; i++)
+		H->ch[i] = 64 + i;
+	new_HFM(H);
+	while ((q = getchar()) != '\n' && q != EOF);
+	scanf("%s", a);
+	scanf("%s", b);
+	for (i = 1; i <= N; i++)
+		printf("%c:%s\n", H->ch[i], H->cd[i]);
+	cd(H, a);//编码
+	key(H, b);//译码
+	free(H);
 	return 0;
 }
+void new_HFM(HBT* H)//建立哈夫曼树
+{
+	int i, j, a, s1, s2;
+	for (i = 1; i <= N; i++)
+	{
+		scanf("%d", &a);
+		H->HFM[i].data = a;
+		H->HFM[i].P = 0;
+		H->HFM[i].L = 0;
+		H->HFM[i].R = 0;
+	}
+	for (i = N + 1; i <= M; i++)
+	{
+		H->HFM[i].data = 0;
+		H->HFM[i].P = 0;
+		H->HFM[i].L = 0;
+		H->HFM[i].R = 0;
+	}
+	for (i = N + 1; i <= M; i++)
+	{
+		min_s1s2(H, i - 1, &s1, &s2);
+		H->HFM[s1].P = i;
+		H->HFM[s2].P = i;
+		H->HFM[i].L = s1;
+		H->HFM[i].R = s2;
+		H->HFM[i].data = H->HFM[s1].data + H->HFM[s2].data;
+	}
+	for (i = 1; i <= N; i++)
+	{
+		s1 = i;
+		for (j = 0; H->HFM[s1].P; j++)
+		{
+			s2 = s1;
+			s1 = H->HFM[s1].P;
+			if (H->HFM[s1].L == s2)
+				H->cd[i][j] = '0';
+			if (H->HFM[s1].R == s2)
+				H->cd[i][j] = '1';
+		}
+		H->cd[i][j] = '\0';
+		strcpy(H->cd[i], strrev(H->cd[i]));
+	}
+}
+void min_s1s2(HBT* H, int n, int* s1, int* s2)//求最小两个值
+{
+	int i;
+	*s1 = 0;
+	*s2 = 0;
+	for (i = 1; !*s1 || !*s2; i++)
+	{
+		if (!*s1 && !H->HFM[i].P)
+		{
+			*s1 = i;
+			continue;
+		}
+		if (*s1 && !H->HFM[i].P)
+			if (H->HFM[i].data < H->HFM[*s1].data)
+			{
+				*s2 = *s1;
+				*s1 = i;
+			}
+			else
+				*s2 = i;
+	}
+	for (i = 1; i <= n; i++)
+	{
+		if (!H->HFM[i].P)
+		{
+			if (H->HFM[i].data < H->HFM[*s1].data)
+			{
+				*s2 = *s1;
+				*s1 = i;
+			}
+			if (H->HFM[i].data < H->HFM[*s2].data && H->HFM[i].data > H->HFM[*s1].data&& i != *s1)
+				*s2 = i;
+		}
+	}
+}
+
+void cd(HBT* H, char a[])//编码
+{
+	int i, j, n;
+	n = strlen(a);
+	for (i = 0; i < n; i++)
+		for (j = 1; j <= N; j++)
+			if (a[i] == H->ch[j])
+				printf("%s", H->cd[j]);
+	printf("\n");
+}
+void key(HBT* H, char b[])//译码
+{
+	int i, j, n;
+	n = strlen(b);
+	for (i = 0, j = M; i <= n; i++)
+	{
+		if (!H->HFM[j].L && !H->HFM[j].R)
+		{
+			printf("%c", H->ch[j]);
+			j = M;
+			//continue;
+		}
+		if (b[i] == '1')
+			j = H->HFM[j].R;
+		else
+			j = H->HFM[j].L;
+	}
+	printf("\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
